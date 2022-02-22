507edf9e281c0396b1be0467aecc406b7a2e2737
aserbezo <83031663+aserbezo@users.noreply.github.com>
2022-02-23 01:21:25 +0200

    Add files via upload

---
+++ 16. Exercise: Workshop/Console Tic-Tac-Toe.py
@@ -0,0 +1,119 @@
+class Player:
+    def __init__(self, name, sign):
+        self.name = name
+        self.sign = sign
+
+
+def read_player():
+    first_player_name = input(f"Player one name: ")
+    second_player_name = input(f"Player two name: ")
+    while True:
+        first_player_sign = input(f"{first_player_name} would you like to play with 'X' or 'O'? ").upper()
+        if first_player_sign == "X" or first_player_sign == "O":
+            break
+
+        # "O" if first_player_sign == "X" else "X" comprehension
+    second_player_sign = "O" if first_player_sign == "X" else "X"
+    # funciata da vrushta dva instancii koito sa za suotvetniq igrach
+    return Player(first_player_name, first_player_sign), Player(second_player_name, second_player_sign)
+
+
+def board_numeration():
+    print("This is the numeration of the board: ")
+    print("| 1 | 2 | 3 |")
+    print("| 4 | 5 | 6 |")
+    print("| 7 | 8 | 9 |")
+
+
+def init_board():
+    size = 3
+    result = []
+    for _ in range(size):
+        result.append([None] * size)
+    return result
+
+
+def is_valid_postiion(board, mapper, position):
+    if position < 1 or position > 9:
+        return False
+    row, col = mapper[position]
+    return board[row][col] is None
+
+
+def print_board(board):
+    for row in board:
+        print("|  ", end="")
+        print("  |  ".join([x if x is not None else ' ' for x in row]), end="")
+        print("  |")
+
+
+def has_won(board, sign):
+    # rows
+    for row in board:
+        # ["X", None, "X"]
+        # [True ,False, True]
+        # ["X", "X", "X"]
+        # [True, True, True]
+        # all fucntion proverqva dali vsichki stetment sa true
+        if all([x == sign for x in row]):
+            return True
+
+    # cols
+    for col in range(len(board)):
+        col_win = True
+        for row in range(len(board)):
+            if board[row][col] != sign:
+                col_win = False
+                break
+        if col_win:
+            return True
+
+    # diagonals
+    left_diaganol_win = True
+    right_diaganol_win = True
+    for idx in range(len(board)):
+        # left diagonals
+        if board[idx][idx] != sign:
+            left_diaganol_win = False
+
+        if board[idx][len(board) - 1 - idx] != sign:
+            right_diaganol_win = False
+
+    return left_diaganol_win or right_diaganol_win
+
+
+first_player, second_player = read_player()
+
+print(board_numeration())
+
+print(f"{first_player.name} starts first! ")
+
+board = init_board()
+board_mapper = {1: [0, 0],
+                2: [0, 1],
+                3: [0, 2],
+                4: [1, 0],
+                5: [1, 1],
+                6: [1, 2],
+                7: [2, 0],
+                8: [2, 1],
+                9: [2, 2],
+                }
+
+turn = 1
+
+while True:
+    current_player = first_player if turn % 2 != 0 else second_player
+    position = int(input(f"{current_player.name} choose a free position [1-9]: "))
+    # proverqvame dali posiciqta e validna i da ne e X or O
+    if not is_valid_postiion(board, board_mapper, position):
+        continue
+
+    row, col = board_mapper[position]
+    board[row][col] = current_player.sign
+
+    print_board(board)
+    if has_won(board, current_player.sign):
+        print(f"{current_player.name} won!")
+        break
+    turn += 1
